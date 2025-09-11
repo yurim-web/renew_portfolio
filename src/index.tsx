884692218b8cb4b3918097de5a343be4c146bb95
lyl5152 <lyl5152@cmcm.co.kr>
2025-09-11 17:06:25 +0900

    전체 폰트 프리텐다드 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/reset.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/reset.css";
+import "./styles/index.css";
+import "./styles/common.css";
+import "./styles/header.css";
+import "./styles/main_section.css";
+import "./styles/info_section.css";
+import "./styles/skills.css";
 
-import App from './App';
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
